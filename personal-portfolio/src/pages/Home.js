c1f260b838b96fb7f5ec2bf034773285fcdad5bf
John Cuevas <jpmcuevas20@gmail.com>
2022-08-22 10:13:48 -0700

    update links on icons home page

--- personal-portfolio/src/pages/Home.js
+++ personal-portfolio/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link} from 'react-router-dom';
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GithubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
@@ -6,15 +7,24 @@ import "../styles/Home.css"
 
 
 function Home() {
+    function open(url) {
+        window.open(url, '_blank').focus();
+    }
     return (
         <div className='home'>
             <div className='about'>
                 <h3>Hello World, My name is John Cuevas</h3>
                 <div className='prompt'>
                     <p>Here is my portfolio</p>
-                    <LinkedInIcon />
-                    <EmailIcon />
-                    <GithubIcon />
+                    <div className='promptIcons'>
+                        <LinkedInIcon onClick={() => {
+                            open("https://www.linkedin.com/in/john-cuevas/");
+                        }} />
+                        <Link to={"/contact"}> <EmailIcon /> </Link>
+                        <GithubIcon onClick={() => {
+                            open("https://github.com/john-cuevas");
+                        }} />
+                    </div>
                 </div>
             </div>
             <div className='skills'>
